22724c6e4164544bf3b1010a8cbf1e8e8e9609e9
Marina_L <lukovnikova.m@bk.ru>
2022-11-17 17:24:07 +0300

    task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+   Console.WriteLine("Большее число " + a); 
+}
+if (a < b)
+{
+    Console.WriteLine("Большее число " + b); 
+}
